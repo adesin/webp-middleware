d81fc0934640a7b9f9ce77e0c788ee98c195eac7
Anton Desin <anton.desin@gmail.com>
2019-04-11 21:49:59 +0300

    Стартовый коммит

---
+++ index.mjs
@@ -0,0 +1,13 @@
+/**
+ * WEBP Middleware
+ *
+ * @author Anton Desin anton.desin@gmail.com
+ * @copyright (c) Anton Desin | Интернет-агентство IT People
+ * @link https://itpeople.ru/
+ */
+
+import WebpMiddleware from "./lib/WebpMiddleware";
+
+export default (options) => {
+  return new WebpMiddleware().create(options);
+};
